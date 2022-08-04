c3cb53a753f1d06f21d32796b628180835e54cb6
Preeyapol Owatsuwan <preeyapol.owa@gmail.com>
2022-08-04 22:04:53 +0700

    new commit

--- myTime/screens/home/HomeScreen.tsx
+++ myTime/screens/home/HomeScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { FlatList, StyleSheet, TouchableOpacity } from 'react-native';
 import { Text, View } from '../../components/Themed';
 import { RootTabScreenProps } from '../../types';
 import { Component } from 'react';
@@ -22,8 +22,47 @@ class HomeScreenClass extends Component {
       dayNamesShort: ['อ.', 'จ.', 'อ.', 'พ.', 'พฤ.', 'ศ.', 'ส.'],
     };
     LocaleConfig.defaultLocale = 'fr'
+    this.state = {
+      data: [
+        { name: "ประชุมกับเพื่อน", isTitle: true, time: "16:40", detail: "คุยเรื่องโปรเจกต์จบ" },
+        { name: "Angular", isTitle: false, time: "8:40", detail: "คุยเรื่องโปรเจกต์จบ" },
+        { name: "ReactJS", isTitle: false, time: "8:40", detail: "คุยเรื่องโปรเจกต์จบ" },
+        { name: "VueJS", isTitle: false, time: "8:40", detail: "คุยเรื่องโปรเจกต์จบ" },
+        { name: "ReactNative", isTitle: false, time: "8:40", detail: "คุยเรื่องโปรเจกต์จบ" },
+        { name: "PHP Frameworks", isTitle: true, time: "8:40", detail: "คุยเรื่องโปรเจกต์จบ" },
+        { name: "Laravel", isTitle: false, time: "8:40", detail: "คุยเรื่องโปรเจกต์จบ" },
+        { name: "CodeIgniter", isTitle: false, time: "8:40", detail: "คุยเรื่องโปรเจกต์จบ" },
+        { name: "CakePHP", isTitle: false, time: "8:40", detail: "คุยเรื่องโปรเจกต์จบ" },
+        { name: "Symfony", isTitle: false, time: "8:40", detail: "คุยเรื่องโปรเจกต์จบ" }
+      ]
+    };
   }
 
+  onclickSeeAll = () => {
+    console.log("onclickSeeAll")
+  }
+
+  onclickItem = (item) => {
+    console.log(item)
+  }
+
+  renderItem = ({ item }) => {
+    return (
+      <TouchableOpacity
+        onPress={() => this.onclickItem(item)}>
+        <View style={styles.item}>
+          <View style={styles.item_circle}></View>
+          <View style={styles.item_content}>
+            <Text style={{ fontSize: 17, fontWeight: "bold", marginBottom: 4 }} >{item.name}</Text>
+            <Text style={{ fontSize: 13, fontWeight: "normal", color: 'grey' }} >{item.detail}</Text>
+          </View>
+          <View style={styles.item_time}>
+            <Text style={{ fontWeight: 'bold', color: (item.isTitle) ? "red" : "black" }}>{item.time} น.</Text>
+          </View>
+        </View>
+      </TouchableOpacity>
+    );
+  };
   render() {
     return (
       <View style={styles.container}>
@@ -43,7 +82,21 @@ class HomeScreenClass extends Component {
               mainColor: '#5463FF',
               textSecondaryColor: '#748DA6',
               borderColor: 'rgba(122, 146, 165, 0.1)', */}
-
+        </View>
+        <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+        <View style={{ backgroundColor: '#f6f6f6', flex: 1 }}>
+          <View style={styles.headerItem}>
+            <Text style={styles.titleEvent}>กิจกรรมในวันที่ 4/08/2022</Text>
+            <TouchableOpacity
+              style={styles.seeAll}
+              onPress={() => this.onclickSeeAll()}>
+              <Text style={{ color: 'grey', fontSize: 12 }}>ดูทั้งหมด</Text>
+            </TouchableOpacity>
+          </View>
+          <FlatList
+            data={this.state.data}
+            renderItem={this.renderItem}
+            keyExtractor={item => item.name} />
         </View>
       </View>
     );
@@ -52,9 +105,54 @@ class HomeScreenClass extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1
   },
   firstView: {
     paddingTop: 44,
-  }
+  },
+  headerItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+  },
+  titleEvent: {
+    marginTop: 20,
+    marginLeft: 10,
+    marginBottom: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  seeAll: {
+    marginLeft: 'auto',
+    marginRight: 10,
+    marginTop: 20,
+  },
+  item: {
+    marginLeft: 10,
+    marginBottom: 10,
+    marginRight: 10,
+    borderRadius: 10,
+    height: 70,
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  item_circle: {
+    width: 14,
+    height: 14,
+    borderRadius: 7,
+    backgroundColor: 'pink',
+    marginLeft: 10,
+  },
+  item_content: {
+    marginLeft: 16,
+    flexDirection: 'column',
+  },
+  item_time: {
+    marginLeft: 'auto',
+    marginRight: 16,
+  },
+  separator: {
+    height: 1,
+    width: '100%',
+  },
 });
